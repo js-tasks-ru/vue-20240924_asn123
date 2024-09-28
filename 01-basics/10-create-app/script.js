6980347e8575a1a5c3931a64d2640c50f0224147
ASN-840-WORK\nesterov <NesterovAS@Loodsen.ru>
2024-09-28 08:39:06 +0300

    create App

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,20 @@
-import { defineComponent, createApp } from 'vue'
+import { defineComponent } from 'vue';
+import { createApp } from 'vue/dist/vue.esm-bundler';
+
+const DateComponent = defineComponent({
+  name: 'DateComponent',
+
+  setup () {
+    const currentDate = new Date().toLocaleDateString(navigator.language, { dateStyle: 'long' });
+
+    return {
+      currentDate,
+    }
+  },
+
+  template: '<div>Сегодня {{ currentDate }}</div>',
+})
+
+
+const app = createApp(DateComponent);
+app.mount('#app');
