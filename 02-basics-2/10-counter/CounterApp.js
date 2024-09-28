9122963daa8cd93cbd218bc504e50dd9df9dab4b
ASN-840-WORK\nesterov <NesterovAS@Loodsen.ru>
2024-09-28 11:21:44 +0300

    counter task

--- 02-basics-2/10-counter/CounterApp.js
+++ 02-basics-2/10-counter/CounterApp.js
@@ -1,25 +1,33 @@
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
+import { ref } from 'vue';
 
 export default defineComponent({
   name: 'CounterApp',
 
-  setup() {},
+  setup() {
+    return {
+      count: ref(0),
+    }
+  },
 
   template: `
     <div class="counter">
       <button
+        @click="count--"
         class="button button--secondary"
         type="button"
         aria-label="Decrement"
-        disabled
+        :disabled="count <= 0"
       >➖</button>
 
-      <span class="count" data-testid="count">0</span>
+      <span class="count" data-testid="count">{{  count  }}</span>
 
       <button
+        @click="count++"
         class="button button--secondary"
         type="button"
         aria-label="Increment"
+        :disabled="count >= 5"
       >➕</button>
     </div>
   `,
